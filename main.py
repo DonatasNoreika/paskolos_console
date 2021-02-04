037cefcd435ed7e6048982a9ae8b7eac2e1494b0
Donatas Noreika <donoras@gmail.com>
2021-02-04 07:40:54 +0200

    Pirma minimali versija (lyg ir viskas veikia)

---
+++ main.py
@@ -0,0 +1,22 @@
+
+from paskola import Paskola
+
+paskola = Paskola(0, 0, 0)
+while True:
+    try:
+        print()
+        pasirinkimas = int(input("Pasirinkite veiksmą:\n1 - įvesti paskolos duomenis,\n2 - parodyti paskolos informaciją\n3 - paskaičiuoti paskolos mokėjimo grafiką\n4 - baigti darbą\n"))
+        if pasirinkimas == 1:
+            suma = int(input("Įveskite paskolos sumą (eurais)\n"))
+            terminas = int(input("Įveskite paskolos atidavimo terminą (mėnesiais)\n"))
+            palukanos = int(input("Įveskite paskolos palūkanų procentą (pvz., 10)\n"))
+            paskola = Paskola(suma, terminas, palukanos)
+        if pasirinkimas == 2:
+            paskola.paskolos_informacija()
+        if pasirinkimas == 3:
+            paskola.mokejimo_grafikas()
+        if pasirinkimas == 4:
+            print("Programa baigta")
+            break
+    except:
+        print("Neteisingas pasirinkimas")
